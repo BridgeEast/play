aa4825243224b3e8e7ce2f572a7e3e799dc4cffd
zhudongqiao <zhudongqiao@zhudongqiao.(none)>
2014-01-16 16:12:39 +0800

    the first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Play::Application.config.secret_key_base = 'f04ffd4e66a6b99fd615a751e011e9102d41769979d2cdac58a84f582489fc35cbb421b1de31c116d161de92f2b625e2aa49425c087db22bc38d694cfa315c15'
